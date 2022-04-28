29d525fbf9e30c6a8ba856388999c072e9becd3f
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2022-04-28 10:14:19 +0200

    cross validation

--- cross_validation/cross_validation.R
+++ cross_validation/cross_validation.R
@@ -7,10 +7,10 @@ performance <- data.frame(matrix(ncol = 2, nrow = 0))
 
 colnames(performance) <- c('rmse', 'rsquared')
 
-for (val in seq(1,10000))
+for (val in seq(1,1000))
 {
 
-sample = dataset[sample(nrow(dataset),17),]; rmse = rmse(sample$Validity, sample$SimpleModel); lm_model = lm(sample$Validity ~ sample$SimpleModel); rsquared = summary(lm_model)$r.squared; print(c(rmse, rsquared))
+sample = dataset[sample(nrow(dataset),22),]; rmse = rmse(sample$Validity, sample$SimpleModel); lm_model = lm(sample$Validity ~ sample$SimpleModel); rsquared = summary(lm_model)$r.squared; print(c(rmse, rsquared))
 
 performance[nrow(performance) + 1,] = c(rmse, rsquared)
 
