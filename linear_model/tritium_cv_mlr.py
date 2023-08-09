e68a4bf852243bf34b9f22e265cbb3882d392893
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2023-08-09 05:26:05 +0200

    start developing the script for the linear model

--- linear_model/tritium_cv_mlr.py
+++ linear_model/tritium_cv_mlr.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 # read dataset
-dataset = pd.readcsv("..\datasets\version_20230808\dataset_selected_20230808.csv", sep = ";")
+dataset = pd.read_csv("..\datasets\version_20230808\dataset_selected_20230808.csv", sep = ";")
 
 print(dataset.tostring())
 
