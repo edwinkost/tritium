527085a1c3047015de2d5b7d62fe56b974410ccb
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2023-08-09 05:56:21 +0200

    start developing the script for the linear model

--- linear_model/tritium_cv_mlr.py
+++ linear_model/tritium_cv_mlr.py
@@ -18,7 +18,7 @@ ano_pet_p_ratio = dataset["pet_p_ratio"] - avg_pet_p_ratio
 ano_dwt_m       = dataset["dwt_m"]       - avg_dwt_m
 # ~ print(ano_pet_p_ratio, ano_dwt_m)
 predictors = pd.DataFrame(ano_pet_p_ratio)
-predictors = predictors.assign(ano_dwt_m)
+predictors["ano_dwt_m"] = ano_dwt_m
 print(predictors)
 
 # ~ # target variable
