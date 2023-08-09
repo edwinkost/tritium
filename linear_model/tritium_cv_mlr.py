76809feb7d6d15dd841bb8c2607db5986fa4cbce
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2023-08-09 05:33:53 +0200

    start developing the script for the linear model

--- linear_model/tritium_cv_mlr.py
+++ linear_model/tritium_cv_mlr.py
@@ -1,8 +1,17 @@
 
 import pandas as pd
+import numpy as np
 
 # read dataset
 dataset = pd.read_csv("../datasets/version_20230808/dataset_selected_20230808.csv", sep = ";")
+# ~ print(dataset.to_string())
 
-print(dataset.to_string())
+# calculate the average values of predictor variables
+avg_pet_p_ratio = dataset["pet_p_ratio"].mean()
+print(avg_pet_p_ratio)
+avg_dwt_m       = dataset["dwt_m"].mean()
+print(avg_dwt_m)
+
+
+
 
