f7daa977159fe6ba722ec2d5582a6396818eb3ea
Edwin Sutanudjaja <edwinkost@UU105378>
2023-08-22 12:04:10 +0200

    test

--- linear_model/tritium_cv_mlr_develop.py
+++ linear_model/tritium_cv_mlr_develop.py
@@ -83,7 +83,7 @@ print(r_squared_all, adj_r_squared_all, rmse_all, mae_all)
 
 
 # cross validation - with LeavePOut
-leaveout = LeavePOut(5)
+leaveout = LeavePOut(10)
 
 # make splits
 leaveout.get_n_splits(predictors)
