9e6878a63f1d184b33977b9a7bd43556094f84cc
Edwin Sutanudjaja <edwinkost@UU105378>
2023-08-25 12:25:38 +0200

    finalizing the validity map

--- model_spatial/plot_validity_maps.sh
+++ model_spatial/plot_validity_maps.sh
@@ -1,20 +1,21 @@
 
 # Note: To run this script, you need PCRaster (https://pcraster.geo.uu.nl/). To install it, see e.g. https://pcraster.geo.uu.nl/pcraster/4.4.0/documentation/pcraster_project/install.html 
 
+# input variables (on a table)
 INPUT_TABLE="../datasets/version_20230812/geospatialmodel_latlon_trimmedparams_20230522_header_lat_lon_dwt_ratio-aet-p_ratio-pet-p.txt"
 
-rm dwt.map ratio_aet_p.map ratio_pet_p.map
-col2map -a --clone cellsize05min.correct.map -x 2 -y 1 -v 3 ${INPUT_TABLE} dwt.map
-col2map -a --clone cellsize05min.correct.map -x 2 -y 1 -v 4 ${INPUT_TABLE} ratio_aet_p.map
-col2map -a --clone cellsize05min.correct.map -x 2 -y 1 -v 5 ${INPUT_TABLE} ratio_pet_p.map
+# convert the input variables to raster maps
+# - remove previous/existing files (if any)
+rm clone.map dwt.map ratio_aet_p.map ratio_pet_p.map
+# - create a clone map (global 5 arcmin)
+mapattr -R 2160 -C 4320 -B -P yb2t -x -180 -y 90 -l 0.083333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333 clone.map
+# - convert table to raster maps
+col2map -a --clone clone.map -x 2 -y 1 -v 3 ${INPUT_TABLE} dwt.map
+col2map -a --clone clone.map -x 2 -y 1 -v 4 ${INPUT_TABLE} ratio_aet_p.map
+col2map -a --clone clone.map -x 2 -y 1 -v 5 ${INPUT_TABLE} ratio_pet_p.map
 
-#~ # simple model
-#~ Parameter	Estimate
-#~ Intercept	-17.8
-#~ pet_p	3.92
-#~ Dwt	0.36
-#~ (pet_p-2.36136)*(dwt-116.15)	-0.08
 
+# plot the validity map
 rm validity_simple*
 pcrcalc validity_simple.map = "
                                -17.8 
@@ -22,6 +23,17 @@ pcrcalc validity_simple.map = "
                             +    0.36*(dwt.map) 
                             +   -0.08*((ratio_pet_p.map-2.36136)*(dwt.map-116.15))
                               "
-gdal_translate validity_simple.map validity_simple.tif
+#~ # simple model
+#~ Parameter	Estimate
+#~ Intercept	-17.8
+#~ pet_p	3.92
+#~ Dwt	0.36
+#~ (pet_p-2.36136)*(dwt-116.15)	-0.08
 
-rm *.xml
+
+# convert the validity map to a tif (and give it a version) 
+gdal_translate validity_simple.map validity_simple_v2023085.tif
+
+
+# remove temporary files
+rm *.xml validity_simple.map dwt.map ratio_aet_p.map ratio_pet_p.map
