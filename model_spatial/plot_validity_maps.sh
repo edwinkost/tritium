330e9865e1f57122c90adb8586814c393fd46007
Edwin Sutanudjaja <edwinkost@UU105378>
2023-08-25 12:29:34 +0200

    finalizing the validity map

--- model_spatial/plot_validity_maps.sh
+++ model_spatial/plot_validity_maps.sh
@@ -1,6 +1,8 @@
 
 # Note: To run this script, you need PCRaster (https://pcraster.geo.uu.nl/). To install it, see e.g. https://pcraster.geo.uu.nl/pcraster/4.4.0/documentation/pcraster_project/install.html 
 
+set -x
+
 # input variables (on a table)
 INPUT_TABLE="../datasets/version_20230812/geospatialmodel_latlon_trimmedparams_20230522_header_lat_lon_dwt_ratio-aet-p_ratio-pet-p.txt"
 
@@ -8,7 +10,7 @@ INPUT_TABLE="../datasets/version_20230812/geospatialmodel_latlon_trimmedparams_2
 # - remove previous/existing files (if any)
 rm clone.map dwt.map ratio_aet_p.map ratio_pet_p.map
 # - create a clone map (global 5 arcmin)
-mapattr -s -R 2160 -C 4320 -B -P yb2t -x -180 -y 90 -l 0.083333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333 clone.map
+mapattr -s -R 2160 -C 4320 -S -P yb2t -x -180 -y 90 -l 0.083333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333 clone.map
 # - convert table to raster maps
 col2map -a --clone clone.map -x 2 -y 1 -v 3 ${INPUT_TABLE} dwt.map
 col2map -a --clone clone.map -x 2 -y 1 -v 4 ${INPUT_TABLE} ratio_aet_p.map
@@ -36,4 +38,6 @@ gdal_translate validity_simple.map validity_simple_v2023085.tif
 
 
 # remove temporary files
-rm *.xml validity_simple.map dwt.map ratio_aet_p.map ratio_pet_p.map
+rm *.xml validity_simple.map dwt.map ratio_aet_p.map ratio_pet_p.map clone.map
+
+set +x
